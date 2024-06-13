aeeb1fdacabffa4a4978b5721f5a9efa3ff7ac53
Neluni Priyangika <96222@fhss.sjp.ac.lk>
2024-06-13 13:29:44 +0530

    SignUp page connected to the backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,20 @@ import LoginPage from './pages/loginPage/LoginPage.jsx';
 import Sigupage from './pages/signupPage/SignupPage.jsx'
 import Homepage from './pages/homePage/HomePage.jsx';
 import PdfViewerpage from './pages/pdfViewerPage/PdfViewerPage.jsx';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route, Navigate} from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<LoginPage/>} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<Sigupage />} />
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/pdfViewer" element={<PdfViewerpage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
--- client/src/pages/signupPage/SignupPage.jsx
+++ client/src/pages/signupPage/SignupPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './signupPage.css';
 import { FaUser,FaLock, FaEnvelope } from "react-icons/fa";
 import { FaUserGear } from "react-icons/fa6";
+import axios from 'axios';
 
 function SignupPage() {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    role: ''
+  });
+
+  const [error, setError] = useState(false)
+
+  const { username, email, password, role } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:5000/api/auth/signup', formData);
+      res.data &&  window.location.replace('/login'); 
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+
   return (
     <div className='wrapper_signup'>
       <div className='from-box signup'>
@@ -11,19 +37,19 @@ function SignupPage() {
           <h1>Sign UP</h1>
 
           <div className='input-box_signup'>
-            <input type='text' placeholder='Username' required/>
+            <input type='text' placeholder='Username' name='username' value={username} onChange={onChange} required/>
             <FaUser className='icon' />
           </div>
           <div className='input-box_signup'>
-            <input type='text' placeholder='Email' required/>
+            <input type='text' placeholder='Email' name='email' value={email} onChange={onChange} required/>
             <FaEnvelope className='icon' />
           </div>
           <div className='input-box_signup'>
-            <input type='password' placeholder='Password' required/>
+            <input type='password' placeholder='Password' name='password' value={password} onChange={onChange} required/>
             <FaLock className='icon' />
           </div>
           <div className='input-box_signup'>
-            <input type='password' placeholder='Role' required/>
+            <input type='text' placeholder='Role' name='role' value={role} onChange={onChange} required/>
             <FaUserGear className='icon' />
           </div>
 
@@ -34,12 +60,12 @@ function SignupPage() {
           
           <div className="remember-forgot_signup">
             <label>
-              <input type='checkbox'/>
+              <input type='checkbox' required/>
               I agree to the tearms and conditions
             </label>
           </div>
 
-          <button type='submit'>Sign Up</button>
+          <button type='submit' onClick={onSubmit}>Sign Up</button>
 
           <div className="signup-link_signup">
             <p>Already have an account? <a href='/#'>Login</a></p>
